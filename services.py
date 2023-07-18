b8024b1ff34fe2effbe925c8acbd61320b324196
chatsdude <ashtekarchaitanya8@gmail.com>
2023-07-18 21:36:34 +0000

    Services for new routes

--- services.py
+++ services.py
@@ -3,6 +3,7 @@ from models import User, Workout
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timedelta
 import jwt
+from sqlalchemy import func, text
 
 
 def register_user(username, email, password):
@@ -66,3 +67,73 @@ def get_workout_history(user_id):
         raise e
     return workouts
 
+def get_user_workout_progress(user_id, exercise_id):
+    try:
+        workout_data = (
+            db.session.query(
+                func.DATE_FORMAT(Workout.workout_datetime, '%Y-%m-%d').label('date'),
+                func.avg(Workout.accuracy).label('average_accuracy'),
+                func.avg(Workout.attempted_reps).label("attempted_reps")
+            )
+            .filter(
+                Workout.user_id == user_id,
+                Workout.exercise_id == exercise_id,
+            )
+            .group_by(text("DATE_FORMAT(workout_datetime, '%Y-%m-%d')"))
+            .order_by(text("DATE_FORMAT(workout_datetime, '%Y-%m-%d')"))
+            .all()
+        )
+
+        # Format the query result into a list of dictionaries
+        formatted_data = [
+            {'date': row[0], 'average_accuracy': row[1], "average_reps": row[2]}
+            for row in workout_data
+        ]
+
+        return formatted_data
+    except Exception as e:
+        raise e
+
+def parse_duration_str(duration_str):
+    # Parse the duration string
+    try:
+        duration_obj = datetime.strptime(duration_str, "%H:%M:%S")
+
+        # Calculate the total duration in seconds
+        duration_mins = timedelta(hours=duration_obj.hour, minutes=duration_obj.minute, seconds=duration_obj.second).total_seconds()/60
+
+        # Convert to integer duration
+        duration_int = int(duration_mins)
+    except Exception as e:
+        raise e
+
+    return duration_int
+
+def get_user_stats(user_id):
+
+    try:
+        workouts = get_workout_history(user_id)
+
+        total_workouts = len(workouts)
+        total_duration = sum(parse_duration_str(workout.duration) for workout in workouts)
+        avg_duration = total_duration / total_workouts if total_workouts else 0
+
+        total_reps = sum(workout.attempted_reps for workout in workouts)
+        avg_reps = total_reps / total_workouts if total_workouts else 0
+
+        total_exercises = len(set(workout.exercise_id for workout in workouts))
+
+        avg_accuracy = sum(workout.accuracy for workout in workouts) / total_workouts if total_workouts else 0
+
+    except Exception as e:
+        raise e
+
+    return {
+        "total_workouts": total_workouts,
+        "total_duration": total_duration,
+        "avg_duration": avg_duration,
+        "total_reps": total_reps,
+        "avg_reps": avg_reps,
+        "total_exercises": total_exercises,
+        "avg_accuracy": avg_accuracy,
+    }
